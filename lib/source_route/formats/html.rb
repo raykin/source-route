099cff8d13029683e3c9a0c373599786db7ea2b5
raykin <raykincoldxiao@gmail.com>
2014-10-20 17:32:45 +0800

    rename results to wrapper to make code more readable

--- lib/source_route/formats/html.rb
+++ lib/source_route/formats/html.rb
@@ -5,17 +5,16 @@ module SourceRoute
   module Formats
     module Html
 
-      # results is instance of Wrapper
-      def self.slim_render(results)
+      def self.slim_render(wrapper)
+        result_config = wrapper.condition.result_config
         template_path = File.expand_path "../html_template.slim", __FILE__
         slim_template = Slim::Template.new(template_path)
 
-        filename = results.condition.result_config[:filename] ||
-          "#{Time.now.strftime('%H%M')}-source-route.html"
-        if results.condition.result_config[:import_return_to_call] and results.condition.has_call_and_return_event
-          results.import_return_value_to_call_results
+        filename = result_config[:filename] || "#{Time.now.strftime('%H%M')}-source-route.html"
+        if result_config[:import_return_to_call] and wrapper.condition.has_call_and_return_event
+          wrapper.import_return_value_to_call_results
         end
-        html_output_str = slim_template.render(results)
+        html_output_str = slim_template.render(wrapper)
         File.open(filename, 'w') do |f|
           f << html_output_str
         end
