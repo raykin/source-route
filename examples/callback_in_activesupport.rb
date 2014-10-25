6732ba43dd2961aefb2d2d2ad23d584dc1a4bbcb
raykin <raykincoldxiao@gmail.com>
2014-10-25 16:45:06 +0800

    add callback example

---
+++ examples/callback_in_activesupport.rb
@@ -0,0 +1,35 @@
+require 'active_support/callbacks'
+
+$:.unshift("~/studio/source_route/lib")
+require 'source_route'
+
+SourceRoute.enable do
+  event :call, :return
+  defined_class 'ActiveSupport::Callbacks', 'PersonRecord'
+  method_id :base_save, :saving_message, :callback
+  result_config.import_return_to_call = true
+end
+
+class Record
+  include ActiveSupport::Callbacks
+  define_callbacks :save
+
+  def base_save
+    run_callbacks :save do
+      puts "- save"
+    end
+  end
+end
+
+class PersonRecord < Record
+  set_callback :save, :before, :saving_message
+
+  def saving_message
+    puts "saving..."
+  end
+end
+
+person = PersonRecord.new
+person.base_save
+
+SourceRoute.build_html_output
