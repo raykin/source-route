d08b7e18c068a1ad227916ef28f582b06d6cbd7d
raykin <raykincoldxiao@gmail.com>
2014-11-24 11:46:02 +0800

    override activerecord instance json string to prevent crash when work together with Rails4.0.(x)

--- lib/source_route/json_overrides/activerecord_associations_association.rb
+++ lib/source_route/json_overrides/activerecord_associations_association.rb
@@ -7,4 +7,13 @@ if defined? ActiveRecord
       JSON.dump(to_s)
     end
   end
+
+  class ActiveRecord::Base
+
+    # dump association can trigger ActiveSupport::JSON::Encoding::CircularReferenceError when use rails ~> 4.0.1
+    # I try other json gems to fix it, but all failed. That's why I override it here.
+    def to_json(options = nil)
+      JSON.dump(to_s)
+    end
+  end
 end
