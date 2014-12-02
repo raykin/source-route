0b845db97fc24e3ef7db0610c62ccac7facc9dbf
raykin <raykincoldxiao@gmail.com>
2014-12-02 13:59:01 +0800

    comment out to_json override on ActiveRecord, it is not a good solution

--- lib/source_route/json_overrides/activerecord_associations_association.rb
+++ lib/source_route/json_overrides/activerecord_associations_association.rb
@@ -12,9 +12,11 @@ if defined? ActiveRecord
 
     # dump association can trigger ActiveSupport::JSON::Encoding::CircularReferenceError when use rails ~> 4.0.1
     # I try other json gems to fix it, but all failed. That's why I override it here.
-    def to_json(options = nil)
-      JSON.dump(to_s)
-    end
+    #
+    # it can affect the json output of rails AR. Not good solution
+    # def to_json(options = nil)
+    #   JSON.dump(to_s)
+    # end
   end
 
 end
