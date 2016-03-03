ce341fe4de6e279570674d203fca320805c00826
raykin <raykincoldxiao@gmail.com>
2016-03-03 14:59:15 +0800

    fix stack loop error for rails

--- lib/source_route/core_ext.rb
+++ lib/source_route/core_ext.rb
@@ -1,7 +1,8 @@
 class Object
   def source_route_display
-    # to_s # cause crash?
-    self
+    # in case inspect was override
+    inspect_method = Kernel.instance_method(:inspect)
+    inspect_method.bind(self).call
   end
 end
 
@@ -20,5 +21,11 @@ end
 class String
   def source_route_display
     eql?('') ? inspect : to_s
+  end
+end
+
+class Numeric
+  def source_route_display
+    self
   end
 end
