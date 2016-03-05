898bcebea072e7320805b5acbdd1ffd4634d85d9
raykin <raykincoldxiao@gmail.com>
2016-03-05 13:45:24 +0800

    cleanup

--- lib/source_route/json_overrides/activerecord_associations_association.rb
+++ lib/source_route/json_overrides/activerecord_associations_association.rb
@@ -22,23 +22,18 @@ if defined? ActiveRecord
     end
 
     class Base
-
       def source_route_display
-      to_s
-    end
+        to_s
+      end
 
-    # dump association can trigger ActiveSupport::JSON::Encoding::CircularReferenceError when use rails ~> 4.0.1
-    # I try other json gems to fix it, but all failed. That's why I override it here.
-    #
-    # it can affect the json output of rails AR. Not good solution
-    # def to_json(options = nil)
-    #   JSON.dump(to_s)
-    # end
-  end
+      # dump association can trigger ActiveSupport::JSON::Encoding::CircularReferenceError when use rails ~> 4.0.1
+      # I try other json gems to fix it, but all failed. That's why I override it here.
+      #
+      # it can affect the json output of rails AR. Not good solution
+      # def to_json(options = nil)
+      #   JSON.dump(to_s)
+      # end
+    end # END Base
 
-
-
-
-
-end
+  end # END ActiveRecord
 end
