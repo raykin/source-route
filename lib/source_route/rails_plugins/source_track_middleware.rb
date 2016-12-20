3615cf2aa2a229bb2c5b4406c6d2713e1cdf4992
raykin <raykincoldxiao@gmail.com>
2016-12-20 11:13:06 +0800

    update middleware

--- lib/source_route/rails_plugins/source_track_middleware.rb
+++ lib/source_route/rails_plugins/source_track_middleware.rb
@@ -3,18 +3,17 @@ module Rack
 
     def initialize(app, opts={})
       @opts = opts
-      if @opts.present?
-        ::SourceRoute.enable do
-          # defined_class 'Cors'
-          # method_id opts[:method_id] # will crashed
-          method_id 'resource_for_path'
-          full_feature
-        end
-      end
       @app = app
     end
 
     def call(env)
+      ::SourceRoute.enable do
+        # defined_class 'Cors'
+        # method_id opts[:method_id] # will crashed
+        method_id 'render'
+        full_feature
+      end
+
       @app.call(env)
     end
 
